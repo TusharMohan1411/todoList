57902e9e87f8e118b85e14b46111080bad75040e
Tushar <tusharmohan0416@gmail.com>
2024-03-29 08:53:47 +0530

    todo list launched

---
+++ todo.js
@@ -0,0 +1,72 @@
+var addTaskBtn = document.querySelector("#addTaskBtn");
+var inputBox = document.querySelector("#inputBox");
+var todoList = document.querySelector("#todoList");
+const today = document.querySelector("#today");
+var edittodothis;
+
+const date = new Date();
+today.innerHTML = date.toDateString();
+
+const editTodo = (e) => {
+    console.log(e.target.innerHTML);
+    if (e.target.innerHTML === "Remove") {
+        todoList.removeChild(e.target.parentElement);
+    }
+
+    if (e.target.innerHTML === "Edit") {
+        inputBox.value = e.target.previousSibling.innerHTML;
+        inputBox.focus();
+        addTaskBtn.innerHTML = "Update";
+        edittodothis = e;
+    }
+}
+
+function todoMain() {
+
+    var inputText = inputBox.value;
+
+
+    if (inputText.length <= 0) {
+        alert("Please add some task before adding.")
+        return false;
+    }
+
+    if (addTaskBtn.innerHTML === "Update") {
+        edittodothis.target.previousSibling.innerHTML = inputBox.value;
+        addTaskBtn.innerHTML = "Add";
+        inputBox.value = "";
+    } else {
+
+        var newTaskli = document.createElement("li");
+
+        const newTaskP = document.createElement("p");
+        newTaskP.classList.add("todoTask");
+
+        const editTask = document.createElement("p");
+        editTask.innerHTML = "Edit";
+        editTask.classList.add("editTask");
+
+        const removeTask = document.createElement("p");
+        removeTask.innerHTML = "Remove";
+        removeTask.classList.add("removeTask");
+
+        newTaskP.innerHTML = inputText;
+
+        newTaskli.appendChild(newTaskP);
+        newTaskli.appendChild(editTask);
+        newTaskli.appendChild(removeTask);
+
+        todoList.appendChild(newTaskli);
+
+        inputBox.value = "";
+    }
+};
+
+todoList.addEventListener("click", editTodo);
+
+addTaskBtn.addEventListener("click", todoMain);
+document.body.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        todoMain();
+    }
+});
